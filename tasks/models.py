2186822055c87dadf813d747f2707e76f0aa3458
pzey2011 <peymanzeynali@yahoo.com>
2017-08-26 22:07:34 +0430

    views added

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Group(models.Model):
 	title = models.CharField(max_length=200)
+	def __str__ (self):
+		return self.name
 
 class Task(models.Model):
 	STATUS = (
@@ -12,5 +14,7 @@ class Task(models.Model):
 	status = models.CharField(max_length=1, choices=STATUS)
 	group = models.ForeignKey(Group, on_delete=models.CASCADE)
 	creation_date = models.DateTimeField('date created')
+	def __str__ (self):
+		return self.name
 
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
+from .models import Task, Group
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+	groups=Group.objects.all()
+	return render(request,'tasks/index.html',{ 'groups' :groups })
